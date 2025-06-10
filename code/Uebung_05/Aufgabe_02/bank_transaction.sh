003ca1a817e133de596f656e0e04ee01667b5fc5
hw <molle.hw@gmail.com>
2025-06-10 14:11:58 +0200

    finish bank transaction

---
+++ code/Uebung_05/Aufgabe_02/bank_transaction.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+trap "" SIGINT
+
+while true; do
+  read -p "Username: " username
+  read -p "Tan: " entered_tan
+
+  directory_name="$username.TAN"
+  filename="$directory_name/tan"
+
+  if ! test -d "$directory_name" || ! test -s "$filename"; then
+    echo "error occured, make sure directory for user exists tans exist"
+    exit 1
+  fi
+
+  #nur der este tan ist valid,
+  valid_tan=$(head -n 1 $filename)
+  
+  if test "$valid_tan" == "$entered_tan"; then
+    echo "acces allowed"
+    tail -n +2 "$filename" > "$filename.tmp" && mv "$filename.tmp" "$filename"
+  fi
+done
